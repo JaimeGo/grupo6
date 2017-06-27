7d405ae8fa22fc0e6b1190c364917c2ae9569975
fringlesinthestreet <bicarcamo@uc.cl>
2017-06-27 17:49:22 -0400

    prueba 132

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -21,7 +21,7 @@ MONGODATABASE = "dbName"
 MONGOSERVER = "localhost"
 MONGOPORT = 27017
 client = MongoClient(MONGOSERVER, MONGOPORT)
-mongodb = client[MONGODATABASE]
+dbName = client[MONGODATABASE]
 
 ''' # Uncomment for postgres connection
 # REPLACE WITH YOUR DATABASE NAME, USER AND PASS
@@ -52,7 +52,7 @@ def home():
 @app.route("/mongo")
 def mongo():
     query = request.args.get("query")
-    results = eval('mongodb.'+query)
+    results = eval('dbName.'+query)
     results = json_util.dumps(results, sort_keys=True, indent=4)
     if "find" in query:
         return render_template('mongo.html', results=results)
