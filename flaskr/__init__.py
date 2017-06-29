1da319161475ca40b3c40b471f277059653fe8d9
fringlesinthestreet <bicarcamo@uc.cl>
2017-06-29 16:33:31 -0400

    mensaje

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -104,7 +104,7 @@ def numero():
             if numero == subdic['numero']:
 
                 resultado_final.append(subdic)
-
+    return '2'
     resultado_final.sort(key = lambda x: datetime.strptime(x['fecha'], "%d-%m-%y"), reverse = True)
                 
     resultado_final = json_util.dumps(resultado_final[:entero], sort_keys=True, indent=4)
